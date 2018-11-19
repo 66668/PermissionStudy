5f5c77e3ae4e3f30a39b371cfb70bba04cc1f6cf
JackSong <sjy0118atsn@163.com>
2018-11-19 20:44:27 +0800

    SYSTEM_ALERT_WINDOW权限优化

--- app/src/main/java/com/sjy/permission/SpecialAct.java
+++ app/src/main/java/com/sjy/permission/SpecialAct.java
@@ -1,18 +1,13 @@
 package com.sjy.permission;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.BindView;
